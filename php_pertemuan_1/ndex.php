fc3bf08249d6a219370d50f84669afbee37d8b69
elissriwahyuni2008-tech <elissriwahyuni2008@gmail.com>
2026-07-31 11:58:10 +0700

    tugas php 1

---
+++ php_pertemuan_1/ndex.php
@@ -0,0 +1,25 @@
+<?php
+
+echo 'Nama : ';
+echo 'Elis sriwahyuni';
+// tipe data string (single quote)
+
+$nama = "eaeaeaeaaa";
+echo "Nama : ";
+echo 'Elis Sriwahyuni';
+echo "\n";
+// string (double quote)
+
+echo "benar";
+echo true;
+
+echo"salah";
+echo false;
+// tipe data boolean
+
+$nama = "Elis";
+$age = 17;
+echo "";
+echo "Nama : $nama";
+echo "Age : $age";
+//variabel php
---
+++ php_pertemuan_1/string.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Nama : ";
+var_dump("Elis sriwahyuni");
+
+echo "Sekolah : ";
+var_dump("SMK Negeri 2 Sumedang");
+
+echo "Belajar PHP";
+echo "\n";
+
+echo "Halo " . "Dunia";
